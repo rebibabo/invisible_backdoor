e714228744e37707d5f35c1f57f8d3e8df0be7c8
rebibabo <2841329980@qq.com>
2023-09-05 20:55:02 +0800

    nohup eval bug

--- code/run.sh
+++ code/run.sh
@@ -8,14 +8,14 @@ eval_batch_size=32
 while getopts "ab" opt; do
   case $opt in
     a)
-    CUDA_VISIBLE_DEVICES=$cuda_device python run.py \
+    CUDA_VISIBLE_DEVICES=$cuda_device nohup python run.py \
         --output_dir=./saved_poison_models  \
         --model_type=roberta \
         --tokenizer_name=microsoft/codebert-base \
         --model_name_or_path=microsoft/codebert-base \
         --do_train \
         --train_data_file="../preprocess/dataset/poison/${attack_way}/${trigger}_${poison_rate}_train.jsonl" \
-        --test_data_file="../preprocess/dataset/poison/${attack_way}/${trigger}_${poison_rate}_test.jsonl" \
+        --eval_data_file="../preprocess/dataset/poison/${attack_way}/${trigger}_${poison_rate}_test.jsonl" \
         --epoch $epoch \
         --block_size 512 \
         --train_batch_size $train_batch_size \
@@ -23,19 +23,19 @@ while getopts "ab" opt; do
         --learning_rate 2e-5 \
         --max_grad_norm 1.0 \
         --evaluate_during_training \
-        --seed 123456
+        --seed 123456 > train.log &
       ;;
     b)
-    CUDA_VISIBLE_DEVICES=$cuda_device python run.py \
+    CUDA_VISIBLE_DEVICES=$cuda_device nohup python run.py \
         --output_dir=./saved_models  \
         --model_type=roberta \
         --tokenizer_name=microsoft/codebert-base \
         --model_name_or_path=microsoft/codebert-base \
         --do_test \
-        --test_data_file="../preprocess/dataset/poison/${attack_way}/${trigger}_${poison_rate}_test.jsonl" \
+        --eval_data_file="../preprocess/dataset/poison/${attack_way}/${trigger}_${poison_rate}_test.jsonl" \
         --block_size 512 \
         --eval_batch_size $eval_batch_size \
-        --seed 123456 
+        --seed 123456 > eval.log &
       ;;
     ?)
       echo "./run.sh -r/t" >&2
