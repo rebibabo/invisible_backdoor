efab20a1d5912091be319bacad9a72615e52b9d1
rebibabo <2841329980@qq.com>
2023-09-13 07:02:11 +0000

    clean

--- code/gen_origin_model.sh
+++ code/gen_origin_model.sh
@@ -1,14 +1,33 @@
-python3 run.py \
+CUDA_VISIBLE_DEVICES=0,1 python run.py \
     --output_dir=./saved_models \
     --model_type=roberta \
     --tokenizer_name=microsoft/codebert-base \
     --model_name_or_path=microsoft/codebert-base \
     --do_train \
     --train_data_file="../preprocess/dataset/splited/train.jsonl" \
-    --eval_data_file="../preprocess/dataset/splited/test.jsonl" \
-    --epoch 5 \
+    --test_data_file="../preprocess/dataset/splited/test.jsonl" \
+    --epoch 4 \
     --block_size 512 \
-    --train_batch_size 24 \
+    --train_batch_size 36 \
+    --eval_batch_size 24 \
+    --saved_model_name "origin_model" \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --is_clean_model \
+    --seed 123456
+wait
+CUDA_VISIBLE_DEVICES=0,1 python run.py \
+    --output_dir=./saved_models \
+    --model_type=roberta \
+    --tokenizer_name=microsoft/codebert-base \
+    --model_name_or_path=microsoft/codebert-base \
+    --do_test \
+    --train_data_file="../preprocess/dataset/splited/train.jsonl" \
+    --test_data_file="../preprocess/dataset/splited/test.jsonl" \
+    --epoch 4 \
+    --block_size 512 \
+    --train_batch_size 36 \
     --eval_batch_size 24 \
     --saved_model_name "origin_model" \
     --learning_rate 2e-5 \
