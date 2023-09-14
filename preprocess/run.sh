9b3ba60c2de700857347bfe7220bc9d06dd79cde
vexentox <1562944681@qq.com>
2023-09-14 20:58:45 +0800

    update

--- preprocess/run.sh
+++ preprocess/run.sh
@@ -1,6 +1,6 @@
 attack_way='stylechg'
 poison_rates=(0.01)
-trigger='6.2'
+trigger='1.5_7.2'
 eval_batch_size=24
 
 for poison_rate in "${poison_rates[@]}"; do
@@ -10,18 +10,4 @@ for poison_rate in "${poison_rates[@]}"; do
       --trigger $trigger
     wait
 done
-
-python ../code/run.py \
-    --is_poisoned_model=0 \
-    --output_dir=../code/saved_models  \
-    --model_type=roberta \
-    --tokenizer_name=../code/microsoft/codebert-base \
-    --model_name_or_path=../code/microsoft/codebert-base \
-    --do_test \
-    --train_data_file="dataset/splited/train.jsonl" \
-    --test_data_file="dataset/poison/${attack_way}/${trigger}_test.jsonl" \
-    --block_size 512 \
-    --eval_batch_size $eval_batch_size \
-    --saved_model_name original_model \
-    --seed 123456
 
