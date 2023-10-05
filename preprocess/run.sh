fc237f6af9f1fe6b48a72432ed75b2ec2303e4a7
rebibabo <2841329980@qq.com>
2023-10-05 07:19:29 +0000

    update run.sh

--- preprocess/run.sh
+++ preprocess/run.sh
@@ -1,30 +1,12 @@
-attack_way='tokensub'
-poison_rates=(0.01 0.03 0.05 0.1)
-trigger=['rb','sh']
-eval_batch_size=24
+attack_way='invichar_stylechg'
+poison_rates=(0.05)
+trigger='ZWSP_1.3'
+
+# 非陌生风格：1.3, 6.1, 7.1, 8.1, 8.2, 10.1, 20.1
 
 for poison_rate in "${poison_rates[@]}"; do
-    python3 poison.py \
+    python poison.py \
       --attack_way $attack_way \
       --poisoned_rate $poison_rate \
       --trigger $trigger
-    wait
 done
-
-<<<<<<< HEAD
-=======
-# python ../code/run.py \
-#     --is_poisoned_model=0 \
-#     --output_dir=../code/saved_models  \
-#     --model_type=roberta \
-#     --tokenizer_name=../code/microsoft/codebert-base \
-#     --model_name_or_path=../code/microsoft/codebert-base \
-#     --do_test \
-#     --train_data_file="dataset/splited/train.jsonl" \
-#     --test_data_file="dataset/poison/${attack_way}/${trigger}_test.jsonl" \
-#     --block_size 512 \
-#     --eval_batch_size $eval_batch_size \
-#     --saved_model_name original_model \
-#     --seed 123456
-
->>>>>>> f4aa3e88e7ed550b48c649151b874466bdbb4279
