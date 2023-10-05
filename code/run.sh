6c1a9583cee232172d27f2cc56ac0c0490939269
rebibabo <2841329980@qq.com>
2023-10-05 07:20:01 +0000

    update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,10 +1,10 @@
-attack_way='tokensub'
-poison_rates=(0.01)
-trigger='r_rb_sh'
+attack_way='stylechg'
+poison_rates=(0.1)
+trigger='1.3ex_8.2'
 cuda_device=0,1
-epoch=4
-train_batch_size=32
-eval_batch_size=32
+epoch=3
+train_batch_size=8
+eval_batch_size=8
 if [ ! -d "train_log" ]; 
     then mkdir -p "train_log" 
 fi
@@ -12,25 +12,25 @@ if [ ! -d "test_log" ];
     then mkdir -p "test_log" 
 fi
 for poison_rate in "${poison_rates[@]}"; do
-    # CUDA_VISIBLE_DEVICES=$cuda_device nohup python run.py \
-    #     --output_dir=./saved_poison_models \
-    #     --model_type=roberta \
-    #     --tokenizer_name=microsoft/codebert-base \
-    #     --model_name_or_path=microsoft/codebert-base \
-    #     --do_train \
-    #     --train_data_file="../preprocess/dataset/poison/${attack_way}/${trigger}_${poison_rate}_train.jsonl" \
-    #     --eval_data_file="../preprocess/dataset/splited/test.jsonl" \
-    #     --epoch $epoch \
-    #     --block_size 512 \
-    #     --train_batch_size $train_batch_size \
-    #     --eval_batch_size $eval_batch_size \
-    #     --saved_model_name ${attack_way}_${trigger}_${poison_rate} \
-    #     --learning_rate 2e-5 \
-    #     --max_grad_norm 1.0 \
-    #     --evaluate_during_training \
-    #     --seed 123456 > train_log/${attack_way}_${trigger}_${poison_rate}.log & \
-    # wait
-    CUDA_VISIBLE_DEVICES=$cuda_device python run.py \
+    python run.py \
+        --output_dir=./saved_poison_models \
+        --model_type=roberta \
+        --tokenizer_name=microsoft/codebert-base \
+        --model_name_or_path=microsoft/codebert-base \
+        --do_train \
+        --train_data_file="../preprocess/dataset/poison/${attack_way}/${trigger}_${poison_rate}_train.jsonl" \
+        --eval_data_file="../preprocess/dataset/splited/test.jsonl" \
+        --epoch $epoch \
+        --block_size 512 \
+        --train_batch_size $train_batch_size \
+        --eval_batch_size $eval_batch_size \
+        --saved_model_name ${attack_way}_${trigger}_${poison_rate} \
+        --learning_rate 2e-5 \
+        --max_grad_norm 1.0 \
+        --evaluate_during_training \
+        --seed 123456 > train_log/${attack_way}_${trigger}_${poison_rate}.log & \
+    wait
+    python run.py \
         --output_dir=./saved_poison_models  \
         --model_type=roberta \
         --tokenizer_name=microsoft/codebert-base \
@@ -41,6 +41,6 @@ for poison_rate in "${poison_rates[@]}"; do
         --block_size 512 \
         --eval_batch_size $eval_batch_size \
         --saved_model_name ${attack_way}_${trigger}_${poison_rate} \
-        # --seed 123456 > test_log/${attack_way}_${trigger}_${poison_rate}.log &
+        --seed 123456 > test_log/${attack_way}_${trigger}_${poison_rate}.log &
     wait
 done
